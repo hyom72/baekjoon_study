bfb335b730ecb246806ba6b1d15d99220c68839a
hyom72 <loveya0702@naver.com>
2021-06-26 21:54:16 +0900

    21.06.26 baekjoon with python

---
+++ Bronze3/2460.py
@@ -0,0 +1,12 @@
+# 백준 문제(2021.6.26)
+# 2460번) https://www.acmicpc.net/problem/2455
+people = 0
+max_people = 0
+
+for i in range(10) :
+    a, b = map(int, input().split())
+    people = people - a
+    people = people + b
+    if(people > max_people) :
+        max_people = people
+print(max_people)
